99d188e4e96d4ef1d595ca888d2ad76b4a5740d2
Ricardo <raguilarn90@gmail.com>
2023-03-16 18:59:04 +0100

    Revenue hotel analysis
    Analyzing some data about revenue hotels and building Power BI dashboard

--- hotel_revenue.sql
+++ hotel_revenue.sql
@@ -1,3 +1,4 @@
+--Are the revenues growing?
 with hotels as (
 	select * from dbo.['2018$']
 	union
@@ -5,10 +6,20 @@ with hotels as (
 	union
 	select * from dbo.['2020$'])
 
-
---Are the revenues growing?
-
 select arrival_date_year, hotel,
 	round(sum((stays_in_week_nights + stays_in_weekend_nights) * adr),2) as revenue
 from hotels
 group by arrival_date_year, hotel
+
+--Join the other tables
+with hotels as (
+	select * from dbo.['2018$']
+	union
+	select * from dbo.['2019$']
+	union
+	select * from dbo.['2020$'])
+
+select * from hotels
+join dbo.market_segment$ on hotels.market_segment = market_segment$.market_segment
+
+use Project
