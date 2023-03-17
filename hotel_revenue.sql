d7a52a1f1f6fa0fc2f90028475573a64051af62d
Ricardo <raguilarn90@gmail.com>
2023-03-17 14:09:04 +0100

    Update hotel_revenue.sql

--- hotel_revenue.sql
+++ hotel_revenue.sql
@@ -20,6 +20,7 @@ with hotels as (
 	select * from dbo.['2020$'])
 
 select * from hotels
-join dbo.market_segment$ on hotels.market_segment = market_segment$.market_segment
+left join dbo.market_segment$ on hotels.market_segment = market_segment$.market_segment
+left join dbo.meal_cost$ on meal_cost$.meal = hotels.meal
 
 use Project
