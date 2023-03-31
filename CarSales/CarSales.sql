ae578076ee71cabbbf932ba7ac3352f72690b2b9
Ricardo <raguilarn90@gmail.com>
2023-03-31 18:04:02 +0200

    Update CarSales.sql
    Some key analysis from a car sales company

--- CarSales/CarSales.sql
+++ CarSales/CarSales.sql
@@ -19,20 +19,73 @@ group by DEALSIZE
 order by Revenue desc
 
 -- What was the best month for sales in an specific year and what was the quantity?
-select MONTH_ID, round(sum(SALES),2) as Revenue, count(ORDERNUMBER) Quantity
+select MONTH_ID, round(sum(SALES),2) as Revenue, count(ORDERNUMBER) as Quantity
 from sales_data_sample
 where YEAR_ID = '2003'
 group by MONTH_ID
 order by Revenue desc
 
-select MONTH_ID, round(sum(SALES),2) as Revenue, count(ORDERNUMBER) Quantity
+select MONTH_ID, round(sum(SALES),2) as Revenue, count(ORDERNUMBER) as Quantity
 from sales_data_sample
 where YEAR_ID = '2004'
 group by MONTH_ID
 order by Revenue desc
 
-select MONTH_ID, round(sum(SALES),2) as Revenue, count(ORDERNUMBER) Quantity
+select MONTH_ID, round(sum(SALES),2) as Revenue, count(ORDERNUMBER) as Quantity
 from sales_data_sample
 where YEAR_ID = '2005'
 group by MONTH_ID
 order by Revenue desc
+
+select * from dbo.sales_data_sample
+
+-- November is the best month, what was the product sold?
+select top 1 PRODUCTLINE, 
+			MONTH_ID, 
+			round(sum(SALES),2) as Revenue, 
+			count(ORDERNUMBER) as Quantity
+from sales_data_sample
+where YEAR_ID = 2003 AND MONTH_ID = 11
+group by PRODUCTLINE, MONTH_ID
+order by Revenue desc
+-- Classic Cars are the best-selling product in the month of november
+
+-- Who is the best customer?
+-- Recency, Frequency, Monetary Value (RFM). Last order date, count of total orders, total spend
+
+drop table if exists #rfm;
+with rfm as
+	(select CUSTOMERNAME,
+		sum(SALES) as Monetary_Value,
+		round(avg(SALES),2) as Average_Sales,
+		count(ORDERNUMBER) as Total_Orders,
+		max(ORDERDATE) as Last_OrderDate,
+		(select max(ORDERDATE) from sales_data_sample) as Max_OrderDate,
+		datediff(DD, max(ORDERDATE), (select max(ORDERDATE) from sales_data_sample)) as Recency
+	from sales_data_sample
+	group by CUSTOMERNAME
+),
+rfm_calc as
+	(select r.*,
+		ntile(4) over (order by Recency desc) rfm_recency,
+		ntile(4) over (order by Total_Orders) rfm_frequency,
+		ntile(4) over (order by Monetary_Value) rfm_monetary
+	from rfm r
+)
+select 
+	c.*, rfm_recency + rfm_monetary + rfm_frequency as rfm_cell,
+	cast(rfm_recency as varchar) + cast(rfm_monetary as varchar) + cast(rfm_frequency as varchar) as rfm_cell_string
+into #rfm
+from rfm_calc c
+
+select CUSTOMERNAME, rfm_recency, rfm_monetary, rfm_frequency,
+	case
+		when rfm_cell_string in (111,112,121,122,123,132,211,212,114,141) then 'lost_customers'
+		when rfm_cell_string in (133,134,143,244,334,343,344) then 'cannot_lose' --Big spenders, better not to lose
+		when rfm_cell_string in (311,411,331) then 'new_customers'
+		when rfm_cell_string in (222,223,233,322) then 'potential_churners'
+		when rfm_cell_string in (323,333,321,422,332,432) then 'active' --Customers who buy often but at a low price point
+		when rfm_cell_string in (433,434,443,444) then 'loyal'
+	end rfm_Segment
+
+from #rfm
